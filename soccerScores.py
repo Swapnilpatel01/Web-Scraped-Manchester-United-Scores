fd979ccadaffb0554347e9931fe5d0ab25d239cb
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2019-08-25 14:42:39 -0500

    Update soccerScores.py

--- soccerScores.py
+++ soccerScores.py
@@ -12,25 +12,25 @@ espn_score = requests.get(url) #access to site using requests library
 soup = BeautifulSoup(espn_score.text, 'html.parser')
 
 
-all_scores = soup.findAll('a')
-scores1 = []
+allScoreAndTeam = soup.findAll('a')
+scoresAndTeams = []
 
-for element in range(23, len(all_scores)-4,2):
+for element in range(23, len(allScoreAndTeam)-4,2):
     #time.sleep(1)
-    scores1.append(all_scores[element].text)
+    scoresAndTeams.append(allScoreAndTeam[element].text)
     #print(all_scores[element].text + ' \n ')
 
 
-home_team = scores1[0::3]
-team_scores = scores1[1::3]
-away_team = scores1[2::3]
+home_team = scoresAndTeams[0::3]
+final_score = scoresAndTeams[1::3]
+away_team = scoresAndTeams[2::3]
 
 with open('scores.csv', 'a') as f:
     f1 = csv.writer(f)
-    n = {'Home' : y, 'Score' : x, 'Away' : z}
+    n = {'Home' : home_team, 'Score' : final_score, 'Away' : away_team}
     df = pd.DataFrame(n)
     df.to_csv('scores.csv')
 
     
     
-#print(df)
+
