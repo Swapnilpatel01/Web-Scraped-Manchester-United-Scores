6a65561991273a39adeda4ab6c04de799cebfbd1
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2019-08-25 14:30:43 -0500

    Add files via upload

---
+++ soccerScores.py
@@ -0,0 +1,36 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
+
+url = 'https://www.espn.com/soccer/team/results/_/id/360/manchester-united'
+espn_score = requests.get(url) #access to site using requests library
+
+
+soup = BeautifulSoup(espn_score.text, 'html.parser')
+
+
+all_scores = soup.findAll('a')
+scores1 = []
+
+for element in range(23, len(all_scores)-4,2):
+    #time.sleep(1)
+    scores1.append(all_scores[element].text)
+    #print(all_scores[element].text + ' \n ')
+
+
+y = scores1[0::3]
+x = scores1[1::3]
+z = scores1[2::3]
+
+with open('scores.csv', 'a') as f:
+    f1 = csv.writer(f)
+    n = {'Home' : y, 'Score' : x, 'Away' : z}
+    df = pd.DataFrame(n)
+    df.to_csv('scores.csv')
+
+    
+    
+#print(df)
